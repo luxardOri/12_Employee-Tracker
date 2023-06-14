dd8d5bd3a683acb4ff65e2e7f6f430bcf91d4bb3
luxardOri <127153726+luxardOri@users.noreply.github.com>
2023-06-14 11:51:21 -0700

    updated role_id in employee tables

--- db/seeds.sql
+++ db/seeds.sql
@@ -17,19 +17,18 @@ VALUES ("Sales Lead",100000,3),
        ("HR Admin",80000,1),
        ("HR Recruitment",75000,1);
 
-
 INSERT INTO employees (first_name,last_name,role_id,manager_id)
-VALUES ("Bob","McBob",1,NULL),
-       ("Tim","Timothy",2,NULL),
-       ("Sookie","Stackhouse",3,NULL),
-       ("Bill","Compton",4,NULL),
-       ("Eric","Northman",5,NULL),
-       ("Pam","De Beaufort",5,5),
+VALUES ("Bob","McBob",9,NULL),
+       ("Tim","Timothy",5,NULL),
+       ("Sookie","Stackhouse",1,NULL),
+       ("Bill","Compton",3,NULL),
+       ("Eric","Northman",7,NULL),
+       ("Pam","De Beaufort",8,5),
        ("Jessica","Hamby",4,4),
-       ("Tara","Thornton",3,3),
-       ("Lafayette","Reynolds",3,3),
-       ("Andy","Bellefleur",1,1),
-       ("Jason","Stackhouse",1,1),
-       ("Hoyt","Fortenberry",1,1),
-       ("Alcide","Herveaux",2,2),
-       ("Sam","Merlotte",2,2);
+       ("Tara","Thornton",2,3),
+       ("Lafayette","Reynolds",2,3),
+       ("Andy","Bellefleur",10,1),
+       ("Jason","Stackhouse",10,1),
+       ("Hoyt","Fortenberry",10,1),
+       ("Alcide","Herveaux",6,2),
+       ("Sam","Merlotte",6,2);
